7492805762eabc73f6607bd7789bb7b33fdac408
Homa <hmajam83@gmail.com>
2025-10-28 09:32:32 +0330

    add answer and question action for users

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,4 +1,6 @@
  export const RECEIVE_USERS="RECEIVE_USERS";
+ export const ADD_ANSWER_USER="ADD_ANSWER_USER";
+ export const ADD_QUESTION_USER="ADD_QUESTION_USER";
 
  export function receiveUsers(users){
   return {
@@ -6,3 +8,19 @@
     users,
   };
  }
+export function addAnswerUser(qid,answer,authedUser){
+  return {
+    type:ADD_ANSWER_USER,
+    qid,
+    answer,
+    authedUser,
+  };
+}
+
+export function addquestionUser({author,id}){
+  return {
+    type:ADD_QUESTION_USER,
+    qid:id,
+    author,
+  };
+}
