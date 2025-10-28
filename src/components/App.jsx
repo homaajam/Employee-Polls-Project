44f6a08a5ef8cfa0cc20c7b29ef7f613d3f118dd
Homa <hmajam83@gmail.com>
2025-10-28 12:32:28 +0330

    update app componen and main file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
+import LoadingBar from "./LoadingBar.jsx";
+import { Fragment } from "react";
 
+function App(props){
 
-function App(){
-  return <h1>Welcome!</h1>;
+  return (
+    <Fragment>
+      <LoadingBar/>
+
+      <h1>Welcome!</h1>
+    </Fragment>
+  );
 }
 export default App;
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './components/App';
+import middleware from './middleware';
+import reducer from './reducers';
+import { createStore } from 'redux';
+import {Provider} from'react-redux';
+import { HashRouter as Router } from "react-router-dom";
+import {configureStore} from '@reduxjs/toolkit';
+
+const store=createStore(reducer, middleware);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+
+      </Router>
+    </Provider>
   </StrictMode>,
 )
