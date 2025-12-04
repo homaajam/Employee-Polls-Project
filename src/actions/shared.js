c5800bce2173e1b29714e9525df50702ad14d5a7
Homa <hmajam83@gmail.com>
2025-12-04 17:54:22 +0330

    adding defualt authed user temporarily

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,12 +1,15 @@
 import {receiveUsers} from "./users";
 import {receiveQuestions} from "./questions";
 import {getInitialData} from "../utils/api";
+import { setAuthedUser } from "./authedUser";
 
 export function handleInitialData(){
   return (dispatch)=>{
     return getInitialData().then(({users, questions})=>{
       dispatch(receiveUsers(users));
       dispatch(receiveQuestions(questions));
+      const defaultUserId = Object.keys(users)[0];
+      dispatch(setAuthedUser(defaultUserId));
     });
   };
 }
